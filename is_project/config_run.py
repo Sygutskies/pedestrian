6b8e6d01961ee7a3e324a6da6c1dc44683d4e2f1
Sygutskies <adamsygut16@gmail.com>
2024-03-25 19:53:58 +0100

    added gitignore

--- is_project/config_run.py
+++ is_project/config_run.py
@@ -17,7 +17,7 @@ datasetloader = DatasetLoaderPhone(dir_path="pedestrian/is_project/dataset_valid
 X_test, y_test = datasetloader.run()
 build_step_phone = Build(model_type="phone", input_shape = (None, frames_number, 12))
 model_phone = build_step_phone.run()
-train_step = TrainStep(epochs=25)
+train_step = TrainStep(epochs=2)
 model_phone = train_step.run(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, log_name=log_name_phone, model=model_phone)
 kpi_step = KpiStep(model=model_phone, weights="./logs/" + log_name_phone + "/weights/best/checkpoint", log_dir="./logs/" + log_name_phone + "/Kpi/conf_matrix", model_type="phone")
 kpi_step.predict(X_test=X_test, y_test=y_test)
@@ -28,7 +28,7 @@ datasetloader = DatasetLoaderMotion(dir_path="pedestrian/is_project/dataset_vali
 X_test, y_test = datasetloader.run()
 build_step_motion = Build(model_type ="motion", input_shape = (None, frames_number, 12))
 model_motion = build_step_motion.run()
-train_step = TrainStep(epochs=25)
+train_step = TrainStep(epochs=2)
 model_motion = train_step.run(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, log_name=log_name_motion, model=model_motion)
 kpi_step = KpiStep(model=model_motion, weights="./logs/" + log_name_motion + "/weights/best/checkpoint", log_dir="./logs/" + log_name_motion + "/Kpi/conf_matrix", model_type="motion")
 kpi_step.predict(X_test=X_test, y_test=y_test)
@@ -37,7 +37,7 @@ make_movie = MakeMovie(phone_model=model_phone,
                        motion_model=model_motion, 
                        phone_weights="./logs/" + log_name_phone + "/weights/best/checkpoint", 
                        motion_weights="./logs/" + log_name_motion + "/weights/best/checkpoint",
-                       video_source="pedestrian/is_project/test_movies/IMG_5315.MOV",
+                       video_source="test_movies/IMG_5315.MOV",
                        frames_number = frames_number,
                        log_name=log_name)
 make_movie.run()
